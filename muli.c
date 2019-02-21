ce5801b691c24fb9248da824af0c2b196ac66fc0
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-02-21 10:13:35 +0530

    Create muli.c

---
+++ muli.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n,d;
+clrscr():
+scanf("%d",&n);
+printf("enter the num");
+scanf("%d",&d);
+printf("num of mul");
+for(i=1;i<=n;i++)
+if(i%d==0)
+printf("%",i);
+getch();
+}
